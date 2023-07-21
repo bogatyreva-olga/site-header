d954fd348e5a11b1b8d306ec7383956f7972d9c2
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-21 15:47:22 +0300

    add js scroll

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-// function scrollToUp() {
-//     window.addEventListener("scroll", () => {
-//         let mobileMenuTop = document.querySelector(".mobile-menu__top");
-//         let mainSelector = document.querySelector("main")
-//             const scrollY = window.scrollY || document.documentElement.scrollTop;
-//             scrollY < 196 ? mobileMenuTop.classList.add("white-mobile-menu") : mobileMenuTop.classList.add("red-mobile-menu");
-//         });
-//
-// }
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//     scrollToUp();
-// })
+function scrollToUp() {
+    let mobileMenuTop = document.querySelector(".mobile-menu__top");
+    let eScroll = 196;
+
+    window.addEventListener("scroll", (evt) => {
+        if (scrollY >= eScroll) {
+            mobileMenuTop.classList.add("white-mobile-menu")
+        } else {
+            mobileMenuTop.classList.remove("white-mobile-menu")
+        }
+
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    scrollToUp();
+});
 
