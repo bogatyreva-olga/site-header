ade010fbd0725b7765a4ae7b290b1346c72bb89f
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-21 12:18:53 +0300

    add text brand to banner

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function scrollToUp() {
-    window.addEventListener("scroll", () => {
-        let mobileMenuTop = document.querySelector(".mobile-menu__top");
-        let mainSelector = document.querySelector("main")
-        if (mainSelector.scrollTop < 196) {
-          mobileMenuTop.style.background = "#ffffff"
-        } else {
-            mobileMenuTop.style.background = "#F43636";
-        }
-    })
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    scrollToUp();
-})
+// function scrollToUp() {
+//     window.addEventListener("scroll", () => {
+//         let mobileMenuTop = document.querySelector(".mobile-menu__top");
+//         let mainSelector = document.querySelector("main")
+//         if (mainSelector.scrollTop < 196) {
+//           mobileMenuTop.style.background = "#ffffff"
+//         } else {
+//             mobileMenuTop.style.background = "#F43636";
+//         }
+//     })
+// }
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//     scrollToUp();
+// })
 
