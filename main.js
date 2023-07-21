4b247d010dd3fb4c8a1883c9f25bf083304abe71
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-21 13:16:59 +0300

    change hover to input

--- main.js
+++ main.js
@@ -2,12 +2,10 @@
 //     window.addEventListener("scroll", () => {
 //         let mobileMenuTop = document.querySelector(".mobile-menu__top");
 //         let mainSelector = document.querySelector("main")
-//         if (mainSelector.scrollTop < 196) {
-//           mobileMenuTop.style.background = "#ffffff"
-//         } else {
-//             mobileMenuTop.style.background = "#F43636";
-//         }
-//     })
+//             const scrollY = window.scrollY || document.documentElement.scrollTop;
+//             scrollY < 196 ? mobileMenuTop.classList.add("white-mobile-menu") : mobileMenuTop.classList.add("red-mobile-menu");
+//         });
+//
 // }
 //
 // document.addEventListener('DOMContentLoaded', () => {
