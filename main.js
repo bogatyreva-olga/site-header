95630daa9a0bd186eef24d04bf4c0cdb0e199d1d
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-24 18:22:23 +0300

    add style for icons bottom

--- main.js
+++ main.js
@@ -4,17 +4,21 @@ function scrollToUp() {
     let path = window.location.href.toString().split(window.location.host)[1];
 
     window.addEventListener("scroll", (evt) => {
-        // if (path === "/") {
-        //     mobileMenuTop.classList.add("red-mobile-menu")
-
-            if (scrollY >= eScroll) {
-                mobileMenuTop.classList.remove("white-mobile-menu")
-                mobileMenuTop.classList.add("white-mobile-menu")
+            if (path === "/") {
+                mobileMenuTop.classList.add("red-mobile-menu")
             } else {
+                mobileMenuTop.classList.add("white-mobile-menu")
+            }
+            if (scrollY <= eScroll) {
                 mobileMenuTop.classList.remove("white-mobile-menu")
                 mobileMenuTop.classList.add("red-mobile-menu")
+
+            } else {
+                mobileMenuTop.classList.add("white-mobile-menu")
+                mobileMenuTop.classList.remove("red-mobile-menu")
             }
-    })
+        }
+    )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
