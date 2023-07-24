01a520b39b219ac4978c3cd93ac5825a1cf2c730
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-24 14:28:35 +0300

    add hover in burger menu button

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ function scrollToUp() {
     let path = window.location.href.toString().split(window.location.host)[1];
 
     window.addEventListener("scroll", (evt) => {
-        if (path === "/") {
-            mobileMenuTop.classList.add("red-mobile-menu")
-        }
-        if (scrollY >= eScroll || path !== "/") {
-            mobileMenuTop.classList.add("white-mobile-menu")
-        } else {
-            mobileMenuTop.classList.remove("white-mobile-menu");
-            mobileMenuTop.classList.add("red-mobile-menu")
-        }
+        // if (path === "/") {
+        //     mobileMenuTop.classList.add("red-mobile-menu")
+
+            if (scrollY <= eScroll) {
+                mobileMenuTop.classList.add("red-mobile-menu")
+            }
+            if (scrollY >= eScroll) {
+                mobileMenuTop.classList.remove("red-mobile-menu")
+                mobileMenuTop.classList.add("white-mobile-menu")
+            }
+
     })
 }
 
