07460aeaee406a60db0453ff9e51b6d2acc23776
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-24 13:28:31 +0300

    change js file

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function scrollToUp() {
     let mobileMenuTop = document.querySelector(".mobile-menu__top");
     let eScroll = 196;
+    let path = window.location.href.toString().split(window.location.host)[1];
 
     window.addEventListener("scroll", (evt) => {
-        if (scrollY >= eScroll ) {
+        if (path === "/") {
+            mobileMenuTop.classList.add("red-mobile-menu")
+        }
+        if (scrollY >= eScroll || path !== "/") {
             mobileMenuTop.classList.add("white-mobile-menu")
-        } else {
-            mobileMenuTop.classList.remove("white-mobile-menu")
         }
     })
 }
